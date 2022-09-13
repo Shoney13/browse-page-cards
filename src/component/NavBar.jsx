6afae134ca36e555bbca7a7e72a1205f9907a840
Shone Jogi <shonejogi2000@gmail.com>
2022-09-13 14:11:32 +0530

    Added Search input and Animation

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./NavBar.module.css";
-import logo from "../assets/beyond-exams-logo.svg";
 import SearchIcon from '@mui/icons-material/Search';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import ProfileIcon from "./ProfileIcon";
 const NavBar = () => {
+  const [isSearchActive, setIsSearchActive] = useState(false);
+  const [searchText, setSearchText] = useState('');
+
   return (
-    <div className={styles.navBarMain}>
-      <img src={logo} alt="Beyond Exam Logo" />
-      <div className={styles.navSearchArea}>
-        <button className={styles.navSearchButton}>
+    <div className={`${styles.navBarMain} ${isSearchActive ? styles.navSearchActive :''}`}>
+      <div className={styles.navBarLogo} />
+      <div className={`${styles.navSearchArea} ${isSearchActive ? styles.navSearchActive :''}`}>
+          <input type="text" className={`${styles.navSearchInput} ${isSearchActive ? styles.navSearchActive :''}`} value={searchText} onChange={(e) => setSearchText(e.target.value)} placeholder='What would you like to learn?'/>
+        <button className={`${styles.navSearchButton} ${isSearchActive ? styles.navSearchActive :''}`} onClick={()=>setIsSearchActive(prevState=>!prevState)}>
           <SearchIcon/>
         </button>
-        <button className={styles.navCourseButton}>Courses</button>
-        <span className={styles.navBarServices}>
+        <button className={`${styles.navCourseButton} ${isSearchActive ? styles.navSearchActive :''}`}>Courses</button>
+        <span className={`${styles.navBarServices} ${isSearchActive ? styles.navSearchActive :''}`}>
           Our Services
           <KeyboardArrowDownIcon/>
         </span>
-        <ProfileIcon/>
       </div>
+        <ProfileIcon/>
     </div>
   );
 };
